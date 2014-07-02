186e1c81a61c8d834b8136bbac7db0642bfef3f7
Julio Olivera <julio.olvr@gmail.com>
2014-07-02 01:33:35 -0300

    Artist page

--- js/router.js
+++ js/router.js
@@ -1,9 +1,19 @@
 MusicPlayer.Router.map(function() {
   this.resource('song', { path: '/song/:song_id'});
+  this.resource('artist', { path: '/artist/:artist_name' });
 });
 
 MusicPlayer.IndexRoute = Ember.Route.extend({
   beforeModel: function() {
     this.transitionTo('song', 1);
   }
+});
+
+MusicPlayer.ArtistRoute = Ember.Route.extend({
+  model: function(params) {
+    // TODO: When not using fixtures anymore, probably findQuery will be better
+    return this.store.find('song').then(function(songs) {
+      return songs.filterBy('artist', params.artist_name);
+    });
+  },
 });
