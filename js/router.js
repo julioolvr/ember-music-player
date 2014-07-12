114f27031e9cef6a760c4e7d5286e1e78740402e
Julio Olivera <julio.olvr@gmail.com>
2014-07-11 23:45:47 -0300

    Fake slow example for artists

--- js/router.js
+++ js/router.js
@@ -8,3 +8,18 @@ MusicPlayer.IndexRoute = Ember.Route.extend({
     return this.store.find('song');
   }
 });
+
+MusicPlayer.ArtistRoute = Ember.Route.extend({
+  model: function(params) {
+    var artist = this.store.find('artist', params.artist_id),
+      slowDown;
+
+    slowDown = new Ember.RSVP.Promise(function(resolve, reject) {
+      setTimeout(resolve, 3000)
+    });
+
+    return Ember.RSVP.all([artist, slowDown]).then(function(results) {
+      return results[0];
+    });
+  }
+});
